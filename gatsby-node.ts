09f459383f2549f6a277af704378e160f03be255
Victor Ndaba <mwangindaba@gmail.com>
2022-08-21 23:32:34 +0300

    feat: configured createPages functionality

---
+++ gatsby-node.ts
@@ -0,0 +1,28 @@
+const path = require("path");
+
+exports.createPages = async function ({ actions, graphql }) {
+  const { data } = await graphql(`
+    query {
+      allFile(filter: { extension: { eq: "mp4" } }) {
+        nodes {
+          extension
+          name
+          relativeDirectory
+          relativePath
+          publicURL
+          prettySize
+        }
+      }
+    }
+  `);
+
+  data.allFile.nodes.forEach((video) => {
+    actions.createPage({
+      path: `/lesson/${video.name}`,
+      component: path.resolve(`src/components/Lesson.tsx`),
+      context: {
+        name: video.name,
+      },
+    });
+  });
+};
