7dfe0ffe0b4a3842519566213cbf5620db4f6541
Victor Ndaba <mwangindaba@gmail.com>
2022-08-22 23:15:12 +0300

    refactor: removed unused pages and updated homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,12 @@ import { graphql, HeadFC, Link } from "gatsby";
 import Layout from "../components/Layout";
 
 const IndexPage = () => {
-  return <Layout>Hello there world</Layout>;
+  return (
+    <div>
+      <section>How to get started with the OCS</section>
+      <section>How to customize OCS settings</section>
+    </div>
+  );
 };
 
 export default IndexPage;
