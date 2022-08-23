3fc5c9a1f2432a2a79a3e894b4c4881cf459d222
Victor Ndaba <mwangindaba@gmail.com>
2022-08-24 00:04:58 +0300

    feat: updated utility helper methods

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,46 @@
-export const refineName = (val: string) => {
+import { navigate } from "gatsby";
+
+export function refineName(val: string) {
   let value = val.replace(/^\d+-/i, "").replace(/^\d\.+/i, "");
   return value;
-};
+}
 
 export function getCurrentLesson() {
   const url = typeof window !== "undefined" ? window.location.href : "";
   return decodeURI(url.split("/")[4]);
+}
+
+export function getLessonIndex(sources: any) {
+  let index = -1;
+  let currentLesson = getCurrentLesson();
+
+  sources.forEach((s: any, i: number) => {
+    if (s.name === currentLesson) {
+      index = i;
+    }
+  });
+  return index;
+}
+
+export function navigateToLesson(direction: string, sources: any) {
+  const index = getLessonIndex(sources);
+
+  switch (direction) {
+    case "next":
+      {
+        if (index >= 0) {
+          const next = sources[index + 1];
+          navigate(`/lesson/${next.name}`);
+        }
+      }
+      break;
+    case "prev": {
+      if (index > 0) {
+        const next = sources[index - 1];
+        navigate(`/lesson/${next.name}`);
+      }
+    }
+  }
 }
 
 export function fetchItem(key: string) {
@@ -14,4 +49,10 @@ export function fetchItem(key: string) {
     return item;
   }
   return {};
+}
+
+export function setItem(key: string, value: any) {
+  if (typeof window !== "undefined") {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
 }
