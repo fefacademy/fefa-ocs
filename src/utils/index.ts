424a342f22c707d95ed9b91b2f45300c4d0c8db4
Victor Ndaba <mwangindaba@gmail.com>
2022-08-23 23:34:17 +0300

    feat: updated the utils with new functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,16 @@ export const refineName = (val: string) => {
   let value = val.replace(/^\d+-/i, "").replace(/^\d\.+/i, "");
   return value;
 };
+
+export function getCurrentLesson() {
+  const url = typeof window !== "undefined" ? window.location.href : "";
+  return decodeURI(url.split("/")[4]);
+}
+
+export function fetchItem(key: string) {
+  if (typeof window !== "undefined") {
+    let item = JSON.parse(localStorage.getItem(key) ?? "{}");
+    return item;
+  }
+  return {};
+}
