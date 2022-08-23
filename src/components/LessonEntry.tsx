4d63a4e43f831f3ab14fb6e635b22bec7b9dc435
Victor Ndaba <mwangindaba@gmail.com>
2022-08-23 23:36:39 +0300

    refactor: extracted and updated the lesson entry component

---
+++ src/components/LessonEntry.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { IconCircleCheck, IconDeviceDesktop } from "@tabler/icons";
+import { Link } from "gatsby";
+import { fetchItem, getCurrentLesson, refineName } from "../utils";
+import slugify from "slugify";
+import { RingProgress, useMantineColorScheme } from "@mantine/core";
+import ContextConsumer from "../lib/context";
+
+export default function LessonEntry({ lesson }: any) {
+  const { colorScheme } = useMantineColorScheme();
+  const isActive = getCurrentLesson() === lesson.name;
+
+  const archived = fetchItem("fefa-ocs-completed");
+  const progress = fetchItem("fefa-ocs-progress-course");
+  const completed = archived.completed ?? [];
+  const isComplete = completed.includes(slugify(lesson.name));
+
+  const [watched] = useState(
+    progress ? progress[slugify(lesson.name)] || 0 : 0
+  );
+
+  return (
+    <ContextConsumer>
+      {({ data, set }) => {
+        const sessionComplete = data.current.completed ?? [];
+        const currentProgress: number = data.current[lesson.name];
+        const completedInSession = sessionComplete.includes(
+          slugify(lesson.name)
+        );
+
+        return (
+          <Link to={`/lesson/${lesson.name}`}>
+            <div
+              className={`w-full flex items-center justify-between  p-3 ${
+                colorScheme === "dark"
+                  ? "hover:bg-teal-900"
+                  : "hover:bg-teal-200"
+              } ${
+                isActive &&
+                `text-teal-400 ${
+                  colorScheme === "dark" ? "bg-teal-800" : "bg-teal-100"
+                }`
+              }`}
+            >
+              <div className="flex items-center space-x-3 w-full">
+                <IconDeviceDesktop size={20} stroke={2} />
+                <span className="w-2/3 text-ellipsis">
+                  {refineName(lesson.name)} ({lesson.prettySize})
+                </span>
+              </div>
+              {isComplete || completedInSession ? (
+                <IconCircleCheck size={33} color="teal" />
+              ) : (
+                <RingProgress
+                  size={30}
+                  thickness={3}
+                  roundCaps
+                  sections={
+                    currentProgress
+                      ? [
+                          {
+                            color: "teal",
+                            value: Math.round(currentProgress),
+                          },
+                        ]
+                      : watched > 0
+                      ? [
+                          {
+                            color: "teal",
+                            value: Math.round(watched),
+                          },
+                        ]
+                      : []
+                  }
+                />
+              )}
+            </div>
+          </Link>
+        );
+      }}
+    </ContextConsumer>
+  );
+}
