37d96d46263b35d82c7d50272405b1a6c9c374e8
Victor Ndaba <mwangindaba@gmail.com>
2022-09-04 22:16:03 +0300

    ci: added semantic-release configuration

---
+++ release.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  branches: "main",
+  repositoryURL: "https://github.com/fefaacademy/fefa-ocs",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+  ],
+};
