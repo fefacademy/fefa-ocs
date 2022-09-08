5346ee3f0db88775ccc578ba1c55057ce33c9dae
Victor Ndaba <mwangindaba@gmail.com>
2022-09-08 04:29:15 +0300

    chore: updated landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,19 +10,22 @@ const IndexPage = () => {
 
   return (
     <div className="w-full h-full p-10 py-14">
-      <h1 className="text-4xl font-medium mb-5">⚓ Ahoy, Kemosabe 👋</h1>
-      <Text color="dimmed" mb={"xl"}>
-        Welcome to the Fefa Academy Offline Course Server, or OCS, as the cool
-        kids call it. The purpose of going through all the trouble of running a
-        server to watch a course is to bring in some perks that wouldn&apos;t be
-        possible were you to use something like VLC. The OCS provides you with{" "}
-        progress tracking, customization options and each lesson comes
-        accompanied with a write-up that will prove useful to you. If this is
-        your first time using the OCS, watch the getting started videos.
-      </Text>
-      <Anchor href="https://fefaacademy.me" target={"_blank"} mt="sm">
-        Visit Fefa Academy website
-      </Anchor>
+      <Card className={classes.card}>
+        <h1 className="text-4xl font-medium mb-5">⚓ Ahoy, Kemosabe 👋</h1>
+        <Text color="dimmed" mb={"xl"}>
+          Welcome to the Fefa Academy Offline Course Server, or OCS, as the cool
+          kids call it. The purpose of going through all the trouble of running
+          a server to watch a course is to bring in some perks that
+          wouldn&apos;t be possible were you to use something like VLC. The OCS
+          provides you with progress tracking, customization options and each
+          lesson comes accompanied with a write-up that will prove useful to
+          you. If this is your first time using the OCS, watch the getting
+          started videos.
+        </Text>
+        <Anchor href="https://fefaacademy.me" target={"_blank"} mt="sm">
+          Visit Fefa Academy website
+        </Anchor>
+      </Card>
       <section className="w-full flex space-x-6 my-8">
         <div className="w-2/3">
           <Ctas />
